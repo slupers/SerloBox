202ea99576ecbf4c34efb5621a9a67619656fb3f
root <root@ubuntu.ubuntu-domain>
2013-04-04 05:15:49 -0500

    Refactored manage and upload template

--- SerloBox/SerloBox/settings.py
+++ SerloBox/SerloBox/settings.py
@@ -75,7 +75,6 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-#STATIC_ROOT = '/home/carlo/djangostack-1.4.5-0/apps/django/django_projects/SerloBox/static/'
 STATIC_ROOT = ''
 
 # URL prefix for static files.
